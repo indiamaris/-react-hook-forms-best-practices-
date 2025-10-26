73db60e8bc1683410929f88e64a77e507b266984
Minindia <indiamaris@Minindias-Mac-mini.local>
2025-10-26 14:41:54 +0100

    creating styles

---
+++ src/forms/adctive-form/AdtiveForm.tsx
@@ -0,0 +1,11 @@
+import styles from './adictive.module.css';
+export const AdtiveForm = () => {
+  return (
+    <div className={styles.adctiveForm}>
+      <h1 className={styles.adctiveFormTitle}>Adtive Form</h1>
+
+      <button className="adctive-button">Adtive Button</button>
+      <button className="adctive-submit">Adtive Submit</button>
+    </div>
+  );
+};
